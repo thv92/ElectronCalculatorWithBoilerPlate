289cbb22d0b33b9035d36c85e98fa16636088442
Thien Vo <vohuythien@utexas.edu>
2017-07-01 00:48:57 -0400

    Made new structure to map sectionName to itemName to price

---
+++ app/reducers/sectionitemprice.js
@@ -0,0 +1,19 @@
+// @flow
+
+
+export default function () {
+    return {
+        "material": {
+            "gold": 2000,
+            "silver": 1000
+        },
+        "color" : {
+            "green": 1000,
+            "black": 1200
+        }, 
+        "size" : {
+            "large": 2000,
+            "small": 1000
+        }
+    };
+}
