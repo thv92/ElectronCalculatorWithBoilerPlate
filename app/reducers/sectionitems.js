8bc02aa6b01b582488031faf4c41fd0b1be9f1f5
Thien Vo <vohuythien@utexas.edu>
2017-07-01 00:48:13 -0400

    Changed structure for items with price to sectionName to list of items

--- app/reducers/sectionitems.js
+++ app/reducers/sectionitems.js
@@ -3,38 +3,8 @@
 
 export default function () {
     return {
-        "material": [
-            {
-                "name": "gold",
-                "price": 2000
-            },
-
-            {
-                "name": "silver",
-                "price": 1000
-            }
-        ],
-        "color": [
-            {
-                "name": "green",
-                "price": 1000
-            },
-
-            {
-                "name": "black",
-                "price": 1200
-            }
-        ],
-        "size": [
-            {
-                "name": "large",
-                "price": 2000
-            },
-
-            {
-                "name": "small",
-                "price": 1000
-            }
-        ]
+        "material": ["gold", "silver"],
+        "color" : ["green", "black"],
+        "size" : ["large", "small"]
     };
 }
